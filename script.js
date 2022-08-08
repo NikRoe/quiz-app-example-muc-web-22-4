1ebd8c6067a355c59cc331e1ad75ca059b0f8615
Niklas Röttger <niklas.roettger@outlook.de>
2022-08-08 17:41:27 +0200

    add hide and show answer button logic

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const button = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer-text"]');
 const bookmark = document.querySelector('[data-js="question-bookmark"]');
 
+let isShown = false;
+
 function handleButtonClick() {
   answer.classList.toggle("hidden");
+  isShown = !isShown;
+
+  button.innerText = isShown ? "Hide answer" : "Show answer";
 }
 
 button.addEventListener("click", handleButtonClick);
